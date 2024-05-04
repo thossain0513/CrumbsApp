dc453c93fef00e476411616228847ef737533252
thossain0513 <thossain0513@gmail.com>
2024-05-04 02:41:33 -0700

    fixed homescreen

--- src/screens/Home/HomeScreen.js
+++ src/screens/Home/HomeScreen.js
@@ -3,14 +3,42 @@ import React from 'react';
 import {StyleSheet, View, Button, Text, Image, TouchableHighlight, ScrollView, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { tw } from 'nativewind';
+import generateImage from '../../helpers'
 
 
 const RecipeCard = ({ item }) => {
+  const [ex_image, setExImage] = useState(null);
+const [loading, setLoading] = useState(true);
+const [error, setError] = useState(null);
+
+useEffect(() => {
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const imgSrc = await generateImage(item.name);
+      setExImage(imgSrc);
+    } catch (error) {
+      console.error('Error fetching image:', error.message);
+      setError(error.message || 'An error occurred');
+    } finally {
+      setLoading(false);
+    }
+  };
+  fetchData();
+}, [item.name]);
+
+if (loading) {
+  return <ActivityIndicator size="large" color="#0000ff" />;
+}
+
+if (error) {
+  return <Text>Error: {error}</Text>;
+}
   return (
     <TouchableHighlight onPress={() => navigate('RecipeScreen')} underlayColor={styles.underlay}>
       <View tw="flex flex-col h-48 justify-end rounded-lg overflow-hidden shadow-md mx-5 px-0 mb-5 mt-5">
         <Image
-        source={ex_image}
+        source={ {uri: imgSrc} }
         tw="absolute w-full h-full object-cover" />
         <View tw="flex h-[25%] justify-center overflow-hidden bg-gray-200 opacity-75">
         <Text tw="text-center font-bold text-l text-opacity-100">{name}</Text>
